90882304f377246c53f064a402864e4fd537d862
zaydme <Zaydme@users.noreply.github.com>
2019-01-24 13:13:55 +0100

    Update letsdoit.js

--- commands/letsdoit.js
+++ commands/letsdoit.js
@@ -23,7 +23,7 @@ class DoCmd extends Command {
     const msg = await message.channel.send(`${this.client.config.emojis.loading} Preparing the setup for you.`);
     await msg.edit("Please answer following questions in order to make your meme. You can say `cancel` to stop the setup.");
 
-    let topText = await this.client.awaitReply(message, "Please tell me text to write on paper.", 60000);
+    let topText = await this.client.awaitReply(message, "Please tell me what text i should put on top of the meme.", 60000);
     if (topText === false) {
       reply("Prompt timed out.");
       return msg.delete();
@@ -34,24 +34,40 @@ class DoCmd extends Command {
       return msg.delete();
     }
 
-    if (topText.length > 50) return reply("Texts must be maximum 50 characters length.");
+    if (topText.length > 21) return reply("Text on the top of the meme must be maximum 21 characters length.");
+
+    let bottomText = await this.client.awaitReply(message, "Please tell me what text i should put on bottom of the meme. If you want to leave blank, type `.`.", 60000);
+    if (bottomText === false) {
+      reply("Prompt timed out.");
+      return msg.delete();
+    }
+
+    if (bottomText.toLowerCase() === "cancel") {
+      reply("Aborted!");
+      return msg.delete();
+    }
+
+    if (topText.length > 21) return reply("Text on the bottom of the meme must be maximum 21 characters length.");
+
+    if (bottomText === ".") bottomText = "";
 
     await msg.edit(`${this.client.config.emojis.loading} Please wait while printing your meme.`);
 
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 10%`);
     const image = await fsn.readFile("./templates/44.png");
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 35%`);
-    topText = this.client.separateText(topText, 14, 20);
+    topText = this.client.separateText(topText, 21, 40);
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 45%`);
+    bottomText = this.client.separateText(bottomText, 21, 40);
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 55%`);
-    const location = await this.client.textLocation(topText.length, 110, 160, false, true, 0, 0.5);
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 75%`);
-    const newMeme = new Canvas(500, 600)
-      .addImage(image, 0, 0, 500, 600)
-      .setColor("#ffffff")
-      .setTextFont('23px Impact')
+    const newMeme = new Canvas(430, 300)
+      .addImage(image, 0, 0, 430, 300)
+      .setColor('#ffffff')
+      .setTextFont('bold 28px Impact')
       .setTextAlign('center')
-      .addText(topText, location.from, location.to)
+      .addText(topText, 215, 30)
+      .addText(bottomText, 215, 270)
       .toBuffer();
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 99%`);
     const attachment = new Discord.Attachment(newMeme, 'image.png');
