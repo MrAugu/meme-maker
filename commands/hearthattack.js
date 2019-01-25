6618fb8d43ea0eaf0b62542df63aae9ea0564cb1
zaydme <Zaydme@users.noreply.github.com>
2019-01-25 21:01:54 +0100

    hearthattack and trump

--- commands/hearthattack.js
+++ commands/hearthattack.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 const { Canvas } = require('canvas-constructor');
 const fsn = require('fs-nextra');
 
-class TrumpthumbsupCmd extends Command {
+class KittyCmd extends Command {
   constructor (client) {
     super(client, {
-      name: "trumpthumbsup",
-      description: "Makes a thumbs up from trump meme.",
+      name: "kitty",
+      description: "Makes a kitty meme.",
       category: "Meme Maker",
       usage: "",
       enabled: true,
@@ -23,7 +23,7 @@ class TrumpthumbsupCmd extends Command {
     const msg = await message.channel.send(`${this.client.config.emojis.loading} Preparing the setup for you.`);
     await msg.edit("Please answer following questions in order to make your meme. You can say `cancel` to stop the setup.");
 
- /*   let topText = await this.client.awaitReply(message, "Please tell me what text i should put on top of the meme.", 60000);
+    let topText = await this.client.awaitReply(message, "Please tell me what text i should put on top of the meme.", 60000);
     if (topText === false) {
       reply("Prompt timed out.");
       return msg.delete();
@@ -34,9 +34,9 @@ class TrumpthumbsupCmd extends Command {
       return msg.delete();
     }
 
-    if (topText.length > 21) return reply("Text on the top of the meme must be maximum 21 characters length.");*/
+    if (topText.length > 21) return reply("Text on the top of the meme must be maximum 21 characters length.");
 
-    let bottomText = await this.client.awaitReply(message, "Please tell me what text i should put on bottom of the meme.", 60000);
+    let bottomText = await this.client.awaitReply(message, "Please tell me what text i should put on bottom of the meme. If you want to leave blank, type `.`.", 60000);
     if (bottomText === false) {
       reply("Prompt timed out.");
       return msg.delete();
@@ -47,32 +47,34 @@ class TrumpthumbsupCmd extends Command {
       return msg.delete();
     }
 
-    if (bottomText.length > 50) return reply("Text on the bottom of the meme must be maximum 50 characters length.");
+    if (topText.length > 21) return reply("Text on the bottom of the meme must be maximum 21 characters length.");
+
+    if (bottomText === ".") bottomText = "";
 
     await msg.edit(`${this.client.config.emojis.loading} Please wait while printing your meme.`);
 
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 10%`);
-    const image = await fsn.readFile("./templates/22.jpg");
+    const image = await fsn.readFile("./templates/35.png");
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 35%`);
-  //  topText = this.client.separateText(topText, 21, 40);
+    topText = this.client.separateText(topText, 21, 40);
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 45%`);
     bottomText = this.client.separateText(bottomText, 21, 40);
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 55%`);
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 75%`);
-    const newMeme = new Canvas(400, 300)
-      .addImage(image, 0, 0, 400, 300)
-      .setColor('#ffffff')
-      .setTextFont('bold 28px Impact')
+    const newMeme = new Canvas(300, 300)
+      .addImage(image, 0, 0, 300, 300)
+      .setColor('#000000')
+      .setTextFont('28px Impact')
       .setTextAlign('center')
-     // .addText(topText, 200, 30)
-      .addText(bottomText, 200, 270)
+      .addText(topText, 150, 30)
+      .addText(bottomText, 150, 270)
       .toBuffer();
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 99%`);
     const attachment = new Discord.Attachment(newMeme, 'image.png');
     await msg.edit(`${this.client.config.emojis.loading} Priniting your meme... 100%`);
     await msg.delete();
-    message.channel.send("Your thumbs up from trump meme is ready:", attachment);
+    message.channel.send("Your kitty meme is ready:", attachment);
   }
 }
 
-module.exports = TrumpthumbsupCmd;
+module.exports = KittyCmd;
