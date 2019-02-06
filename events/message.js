9c591f575e3f45d74f8d0b23abc22a5adcf37fc4
zaydme <Zaydme@users.noreply.github.com>
2019-02-06 22:28:36 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -25,7 +25,7 @@ module.exports = class {
 
     const level = await this.client.permlevel(message);
 
-    if (cmd.conf.enabled === false) return reply("This command is currently globally disabled.");
+    if (cmd.conf.enabled === false) return reply(":x: **This command is currently globally disabled.**");
 
     if (cmd.conf.args === true && !args.length) {
       return reply(`You haven't provided any arguments. Correct Usage: \`${this.client.config.prefix}${cmd.help.name} ${cmd.help.usage}\``);
@@ -47,7 +47,7 @@ module.exports = class {
         
       if (now < expirationTime) {
         const timeLeft = (expirationTime - now) / 1000;
-        return reply(`Slow it down dude. You have to wait ${timeLeft.toFixed(1)} seconds before using \`${cmd.help.name}\` again.`);
+        return reply(`:raised_hand: Slow it down dude.\n**You have to wait ${timeLeft.toFixed(1)} seconds before using **\`${cmd.help.name}\` **again.**`);
       }
         
       timestamps.set(message.author.id, now);
@@ -55,14 +55,14 @@ module.exports = class {
     }
       
 
-    if (cmd && !message.guild && cmd.conf.guildOnly) return message.channel.send("This command is unavailable via private message. Please run this command in a server.");
+    if (cmd && !message.guild && cmd.conf.guildOnly) return message.channel.send(":x: This command is unavailable via private message.\n**Please run this command in a server.**");
 
     const noPermEmbed = new Discord.RichEmbed()
       .setTitle("FORBIDDEN!")
       .setColor("RED")
       .setAuthor(message.author.tag, message.author.avatarURL)
       .setDescription(`
-Forbidden! You do not have the required permissions to use \`${cmd.help.name}\`.
+:octagonal_sign: **Forbidden!** You do not have the required permissions to use \`${cmd.help.name}\`.
 
 Required Permission Level: ${this.client.levelCache[cmd.conf.permLevel]} - ${cmd.conf.permLevel}
 Your Permission Level: ${level} - ${this.client.config.permLevels.find(l => l.level === level).name}
@@ -82,12 +82,12 @@ Your Permission Level: ${level} - ${this.client.config.permLevels.find(l => l.le
       await cmd.run(message, args, level, reply);
       message.channel.stopTyping(true);
     } catch (e) {
-      reply(`${this.client.config.emojis.fp} Oops seems like these was an error executing command. We tracked error and we will get right into it. (Error: ${e})`);
+      reply(`${this.client.config.emojis.fp} **Oops seems like these was an error executing command.** We tracked error and we will get right into it.\n(Error: ${e})`);
       message.channel.stopTyping(true);
       const errEmbed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.displayAvatarURL)
-        .setTitle("An Error Occured")
-        .setDescription(`An error occured while running \`${cmd.help.name}\`. The error its: \n\`\`\`xl\n${e}\n\`\`\``)
+        .setTitle("**An Error Occured!**")
+        .setDescription(`**An error occured** while running \`${cmd.help.name}\`.\n The error is: \n\`\`\`xl\n${e}\n\`\`\``)
         .setColor("RED")
         .setTimestamp();
       // this.client.channels.get("537745563094089740").send(errEmbed);
